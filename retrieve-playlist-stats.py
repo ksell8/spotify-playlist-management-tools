7516b5398f54e3363fb625ab6bb1b5b90fbebcfe
Kay Sell <ksell@u.rochester.edu>
2020-05-26 00:04:26 -0400

    migrate to click from argparse, add requirements.txt

--- retrieve-playlist-stats.py
+++ retrieve-playlist-stats.py
@@ -1,5 +1,7 @@
-import json, sys, argparse, os
+import json, sys, click, os
 
+@click.command()
+@click.argument('playlist')
 def search(playlist):
     if not os.path.exists("names-of-playlists.json"):
         raise Exception("`python3 get-playlist-data.py names` must be run before this program can execute.")
@@ -11,18 +13,15 @@ def search(playlist):
         stats_dict = json.load(f)
 
     if playlist in names_dict.keys():
+        print(playlist+ " has the following stats:")
         id = names_dict[playlist]
         if id in stats_dict.keys():
             print(stats_dict[id])
-            return
+            return 0
         print("Playlist not in stats JSON.  Try rerunning `python3 get-playlist-data.py stats`")
-        return
+        return 1
     print("Playlist not found.")
+    return 1
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Query for the average statistics per playlist.")
-    parser.add_argument('playlist')
-    args = parser.parse_args()
-
-    print(args.playlist + " has the following stats:")
-    search(args.playlist)
+    search()
